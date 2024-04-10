8d52ec4ac308a8625c3f748204c3645f50ec0ddd
minturtle <minseok0425@kakao.com>
2024-04-10 12:54:22 +0900

    Feat : OCR 기능 API화 완료

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 import os
-
+from ocr import OCRProcessor
 app = Flask(__name__)
 
 app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # 5MB 제한
 
-@app.route('/to-text', methods=['POST'])
+ocr_model = OCRProcessor()
+
+@app.route('/api/to-text', methods=['POST'])
 def image_to_text():
     if 'file' not in request.files:
         return jsonify({"error": "No file part"}), 400
@@ -17,16 +19,15 @@ def image_to_text():
 
     # 파일 확장자 검증
     _, file_extension = os.path.splitext(file.filename)
-    if file_extension.lower() != '.pdf':
+    if file_extension.lower() != '.png':
         return jsonify({"error": "Invalid file type. Only PDF files are allowed."}), 400
 
     # 파일을 스트림으로 읽기
-
+    extracted_texts = " ".join(ocr_model.process_image(file.read()))
 
 
     # 예시 응답
-    return jsonify({"data": result}), 200
-
-    return jsonify({"error": "Unknown error occurred"}), 500
+    return jsonify({"data": extracted_texts}), 200
 
 if __name__ == '__main__':
+    app.run(debug=True)
--- ocr.py
+++ ocr.py
@@ -1,25 +1,30 @@
 import easyocr
-from PIL import Image
-import io
+import easyocr
 import numpy as np
+import io
+from PIL import Image
+
 
 class OCRProcessor:
-    def __init__(self, language_code, font_path="NanumGothic-Regular.ttf", font_size=15):
+    def __init__(self, font_path="NanumGothic-Regular.ttf", font_size=15):
         self.font_path = font_path
         self.font_size = font_size
         # 여기서 GPU 사용 여부를 설정할 수 있습니다. 시스템에 따라 False로 설정할 필요가 있을 수 있습니다.
-        self.reader = easyocr.Reader([language_code], gpu=True)
+        self.reader = easyocr.Reader(['en', 'ja'], gpu=True)
 
     def process_image(self, image_bytes):
-
         # 이미지에서 텍스트 추출
         image = Image.open(io.BytesIO(image_bytes))
         np_image = np.array(image)
         result = self.reader.readtext(np_image)
-
 
         # 추출된 텍스트만을 리스트로 컴파일
         extracted_texts = [text[1] for text in result]
 
         return extracted_texts
 
+# 사용 예시
+if __name__ == "__main__":
+    ocr_processor = OCRProcessor("en")
+    extracted_texts = ocr_processor.process_image('img.png')
+    print(extracted_texts)
